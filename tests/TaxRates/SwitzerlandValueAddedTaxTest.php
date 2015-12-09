60080e03e73e27780aa10261988e9dd79e057045
Dennis Oderwald <oderwald@comyo-media.de>
2015-12-09 11:53:22 +0100

    Add Switzerland TaxRates & Jurisdictions

---
+++ tests/TaxRates/SwitzerlandValueAddedTaxTest.php
@@ -0,0 +1,26 @@
+<?php namespace PhilipBrown\Basket\Tests\TaxRates;
+
+use PhilipBrown\Basket\TaxRates\SwitzerlandValueAddedTax;
+
+class SwitzerlandValueAddedTaxTest extends \PHPUnit_Framework_TestCase
+{
+    /** @var TaxRate */
+    private $rate;
+
+    public function setUp()
+    {
+        $this->rate = new SwitzerlandValueAddedTax;
+    }
+
+    /** @test */
+    public function should_return_rate_as_float()
+    {
+        $this->assertEquals(0.8, $this->rate->float());
+    }
+
+    /** @test */
+    public function should_return_rate_as_percentage()
+    {
+        $this->assertEquals(8, $this->rate->percentage());
+    }
+}
